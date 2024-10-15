569dc5d46647053a0662c1a506109cd6807a0e27
Janak-git <janakmittal123@gmail.com>
2024-10-15 22:29:22 +0530

    Tower Of Hanoi

---
+++ TowerOfHanoi.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+void tower(int n,char a,char b,char c){
+    if(n==0) return;
+    tower(n-1,a,c,b);
+    cout<<a<<"->"<<c<<endl;
+    tower(n-1,b,a,c);
+
+}
+
+int main(){
+    int n;
+    cin>>n;
+    tower(n,'A','B','C');
+    return 0;
+}
