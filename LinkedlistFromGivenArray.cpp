cc78272e151cc1ab631995bc7638ca3f3c449dd7
Janak-git <janakmittal123@gmail.com>
2025-01-04 22:28:42 +0530

    Creating Linked list From Given Array

---
+++ LinkedlistFromGivenArray.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+using namespace std;
+
+struct Node {
+    int data;
+    Node* next;
+    Node(int d)
+    {
+        data = d;
+        next = NULL;
+    }
+};
+
+Node* insertEnd(Node* root, int item)
+{
+    Node* temp = new Node(item);
+    if (root == NULL)
+        return temp;
+  
+    Node* last = root;
+    while (last->next != NULL) {
+        last = last->next;
+    }
+  
+    last->next = temp;
+    return root;
+}
+
+Node* arrayToList(int arr[], int n)
+{
+    Node* root = NULL;
+    for (int i = 0; i < n; i++) {
+        root = insertEnd(root, arr[i]);
+    }
+    return root;
+}
+
+void display(Node* root)
+{
+    while (root != NULL) {
+        cout << root->data << " ";
+        root = root->next;
+    }
+}
+
+// Driver code
+int main()
+{
+    int arr[] = { 1, 2, 3, 4, 5 };
+    int n = sizeof(arr) / sizeof(arr[0]);
+    Node* root = arrayToList(arr, n);
+    display(root);
+    return 0;
+}
